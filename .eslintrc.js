f295c029f1694b86d7b6f8555ab5fbb94d0fb6c4
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2025-01-21 16:19:21 +0100

    feat: added .eslintrc and prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+        'expo',
+        'prettier',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-native/all',
+    ],
+    plugins: [
+        'prettier',
+        '@react-native',
+        'unused-imports',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react-native/sort-styles': 'off',
+        'react-native/no-color-literals': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'no-restricted-imports': [
+            'error',
+            {
+                paths: [
+                    {
+                        name: 'react-native-gesture-handler',
+                    },
+                ],
+            },
+        ],
+    },
+};
