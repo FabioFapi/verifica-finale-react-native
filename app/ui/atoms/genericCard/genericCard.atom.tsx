1bf5aa9fa25fee254a3d0581c31c8b03fb22844f
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2025-01-21 17:30:14 +0100

    feat: added notificationsScreen and fix seetingsScreen

--- app/ui/atoms/genericCard/genericCard.atom.tsx
+++ app/ui/atoms/genericCard/genericCard.atom.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 import { styles } from './genericCard.styles';
 
 interface Props {
@@ -7,34 +7,31 @@ interface Props {
   subTitle: string;
   backgroundColor: string;
   image: any;
+  onPress?: () => void;
 }
 
-export const GenericCard = ({ title, subTitle, backgroundColor, image }: Props) => {
+export const GenericCard = ({ title, subTitle, backgroundColor, image, onPress }: Props) => {
   return (
-    <View
+    <TouchableOpacity
+      disabled={!onPress}
       style={[
         styles.container,
-        {
-          backgroundColor: backgroundColor ?? 'aqua',
-        },
-      ]}>
-      {/* IMAGE */}
+        { backgroundColor: backgroundColor ?? '#fff' },
+      ]}
+      onPress={onPress}
+      activeOpacity={0.8}>
       <View style={styles.containerImage}>
         <Image
           source={image ?? require('../../../../assets/images/logo.jpg')}
           style={styles.image}
         />
       </View>
-      {/* IMAGE */}
 
-      {/* DESCRIPTION */}
+
       <View style={styles.containerDescription}>
         <Text style={styles.title}>{title ?? 'Titolo mancante'}</Text>
-        <View style={styles.containerSubtTitle}>
-          <Text>{subTitle ?? 'Descrizione mancante'}</Text>
-        </View>
+        <Text style={styles.subTitle}>{subTitle ?? 'Descrizione mancante'}</Text>
       </View>
-      {/* DESCRIPTION */}
-    </View>
+    </TouchableOpacity>
   );
 };
