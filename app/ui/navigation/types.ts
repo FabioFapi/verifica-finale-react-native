77775e40c3b414af1b6dbb5ceaa8be3da8d7371c
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2025-01-21 16:47:11 +0100

    feat: renamed all screenPage and added cards in home.screen

--- app/ui/navigation/types.ts
+++ app/ui/navigation/types.ts
@@ -1,25 +1,18 @@
 export enum Screen {
-  HomeMain = 'HomeMain',
   Home = 'Home',
   Detail = 'Detail',
-  Search = 'Search',
+  Settings = 'Settings',
 }
 
 export type TabParams = {
   [Screen.Home]: undefined;
-  [Screen.Detail]: {
-    item: string;
-  };
-};
-
-export type HomeStackParamList = {
-  [Screen.HomeMain]: {
-    text: string;
-  };
-  [Screen.Search]: undefined;
+  [Screen.Settings]: undefined;
 };
 
 
 export type MainParamList = {
   TabNavigator: undefined;
+  [Screen.Detail]: {
+    id: string;
+  };
 };
